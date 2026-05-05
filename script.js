5b85c6bd5a5c699fe6dfd8103a3b9f7ebbb57c4c
auraline-e <asdf.movie.1984.fnaf@gmail.com>
2026-05-05 16:33:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// intro logic
+const intro = document.getElementById('intro');
+
+if(localStorage.getItem('visited')){
+  if(intro) intro.style.display = 'none';
+}else{
+  localStorage.setItem('visited', 'true');
+  setTimeout(() => {
+    if(intro){
+      intro.style.opacity = '0';
+      intro.style.transition = '0.5s';
+      setTimeout(()=> intro.remove(), 500);
+    }
+  }, 1200);
+}
+
 // scroll animations
 const elements = document.querySelectorAll('.fade-in');
 
